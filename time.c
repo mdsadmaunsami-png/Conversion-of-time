0a60e42aa2c230a0c09cbc95f89de1524f474616
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-01-19 21:34:13 +0600

    Update time.c

--- time.c
+++ time.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
-
 int main() 
 {
     int S,d,h,m,s,rs;
     printf("Enter Second: ");
     scanf("%d", &S);
     d=S/86400;
-    h=S/3600;
-    rs=S%3600;
+    rs=S%86400;
+    
+    h=rs/3600;
+    rs=rs%3600;
+    
     m=rs/60;
     s=rs%60;
-    printf("%d days %d hours %d minutes %d      Second",d,h,m,s);
+    printf("%d days %d hours %d minutes %d seconds",d,h,m,s);
     return 0;
 }
