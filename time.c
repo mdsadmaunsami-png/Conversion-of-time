64d0a5a7a0b5b117c4b310e349ef05860229144f
MD SAD MAUN SAMI <mdsadmaunsami@gmail.com>
2026-01-19 13:05:44 +0600

    time.c

--- time.c
+++ time.c
@@ -2,13 +2,14 @@
 
 int main() 
 {
-    int tsec,h,m,s;
-    printf("Enter total second=");
-    scanf("%d",&tsec);
-    h=tsec/3600;
-    int remaining_sec=tsec%3600;
-    m=remaining_sec/60;
-    s=remaining_sec%60;
-    printf("Hour=%d\nMinute=%d\nSecond=%d",h,m,s);
+    int S,d,h,m,s,rs;
+    printf("Enter Second: ");
+    scanf("%d", &S);
+    d=S/86400;
+    h=S/3600;
+    rs=S%3600;
+    m=rs/60;
+    s=rs%60;
+    printf("%d days %d hours %d minutes %d      Second",d,h,m,s);
     return 0;
 }
